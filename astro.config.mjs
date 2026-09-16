6a332268b019aea8b39aeb64e98fcff63573257e
ATITYA RAM S <atityaramsureshmanickam@gmail.com>
2026-09-16 14:47:01 +0530

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 
 export default defineConfig({
+  site: 'https://jipsy-danger.github.io',
+  base: '/geo-tag',
   integrations: [solid()],
 });
